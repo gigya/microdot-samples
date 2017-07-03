833e99e489409ec1cc5d2a494ee11ecfaf7df0f0
Daniel Lamberger <daniel.lamberger@gigya-inc.com>
2017-07-03 23:18:04 +0300

    Accounts service

---
+++ AccountsService/AccountsService/AccountsService.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using AccountsService.Interface;
+using Orleans;
+using Orleans.Concurrency;
+
+namespace AccountsService
+{
+    public interface IAccountsServiceGrain : IAccountsService, IGrainWithIntegerKey { }
+
+
+    [Reentrant]
+    [StatelessWorker]
+    public class AccountsService : Grain, IAccountsServiceGrain
+    {
+        public async Task SaveAccount(Account account)
+        {
+            await GrainFactory.GetGrain<IAccountGrain>(account.Email).SaveAccount(account);
+        }
+
+        public async Task<Account> GetAccount(string email)
+        {
+            return await GrainFactory.GetGrain<IAccountGrain>(email).GetAccount();
+        }
+
+        public async Task<bool> CheckPassword(string email, string password)
+        {
+            return await GrainFactory.GetGrain<IAccountGrain>(email).CheckPassword(password);
+        }
+
+    }
+}
