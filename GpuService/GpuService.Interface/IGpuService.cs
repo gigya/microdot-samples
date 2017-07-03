5181854c2ee421126fa1b0bdb3238ce5464a3ea3
Daniel Lamberger <daniel.lamberger@gigya-inc.com>
2017-07-03 17:05:15 +0300

    caching

--- GpuService/GpuService.Interface/IGpuService.cs
+++ GpuService/GpuService.Interface/IGpuService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Gigya.Common.Contracts.HttpService;
+using Gigya.Common.Contracts.Attributes;
 
 namespace GpuService.Interface
 {
@@ -8,6 +9,7 @@ namespace GpuService.Interface
     [HttpService(basePort: 10000)]
     public interface IGpuService
     {
+        [Cached]
         Task<byte[]> Hash(string algorithm, int rounds, byte[] input);
         Task<Tuple<byte[], byte[]>> Encrypt(string algorithm, byte[] key, byte[] input);
         // ...
