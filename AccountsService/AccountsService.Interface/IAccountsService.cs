488a71cf7daeaa33978c35e9ab7d9f650edc42c5
Daniel Lamberger <daniel.lamberger@gigya-inc.com>
2017-07-04 18:05:20 +0300

    .

--- AccountsService/AccountsService.Interface/IAccountsService.cs
+++ AccountsService/AccountsService.Interface/IAccountsService.cs
@@ -8,8 +8,13 @@ namespace AccountsService.Interface
     [HttpService(basePort: 30000)]
     public interface IAccountsService
     {
+        [PublicEndpoint("acc.saveAccount")]
         Task SaveAccount(Account account);
+
+        [PublicEndpoint("acc.getAccount")]
         Task<Account> GetAccount(string email);
+
+        [PublicEndpoint("acc.checkPassword")]
         Task<bool> CheckPassword(string email, string password);
     }
 
